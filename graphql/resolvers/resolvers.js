1e5854c044b9630efd502afb4881b0bb44a421a2
David Thorpe <davidthorpe878@gmail.com>
2018-06-05 22:10:47 +0100

    removed a console.log

--- graphql/resolvers/resolvers.js
+++ graphql/resolvers/resolvers.js
@@ -1,16 +1,8 @@
 
 import Resolutions from '../../database/models/resolutions';
 
-const res = Resolutions.find({})
-  .then((response) => {
-    console.log('response', response)
-    return response;
-  }).catch(() => {
-    throw error;
-  });
-
+// These two lines added a test resolution to the database
 // const testResolution = new Resolutions({ _id: '56785695', name: 'Tester' });
-
 // testResolution.save();
 
 export default {
